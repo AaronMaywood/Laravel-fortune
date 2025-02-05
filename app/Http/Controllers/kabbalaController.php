ad78ea5314f869be0b1b5163cca7117e800b4207
AaronMaywood <95473446+AaronMaywood@users.noreply.github.com>
2025-02-05 12:31:04 +0900

    ビジネスロジックをモデルに分離
    また、関数にすることでユニットテストをしやすくした

--- app/Http/Controllers/kabbalaController.php
+++ app/Http/Controllers/kabbalaController.php
@@ -24,20 +24,12 @@ class kabbalaController extends Controller
         $fortunes = Fortune::get();
 
         // 誕生数を計算する
-        // Step1. 誕生日の各桁を足す 例) 1970220 = 1 + 9 + 0 + 2 + 2 + 0 =  14
-        $year = $request->input('year');
-        $month = $request->input('month');
-        $day = $request->input('day');
-        $ymd = str_split($year . $month . $day);
-        $ymd2 = 0;
-        foreach($ymd as $number){
-            $ymd2 += $number;
-        }
-        // Step2. 9 で割った余りが誕生数、ただし0の場合は9とする
-        $birthNumber = $ymd2 % 9;
-        if ($birthNumber === 0) {
-            $birthNumber = 9;
-        }
+        $birthNumber = Fortune::calcBirthNumber
+        (
+            $request->input('year'),
+            $request->input('month'),
+            $request->input('day'),
+        );
 
         // 占いの決定
         $fortune = $fortunes[--$birthNumber];
